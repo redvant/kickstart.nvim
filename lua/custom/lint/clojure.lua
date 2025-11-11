31b1392dfd1a32302d627f7fe292186cf569ba55
Edgar Roman Cervantes <redvant@outlook.com>
2025-11-10 22:31:46 -0600

    Add Clojure setup

---
+++ lua/custom/lint/clojure.lua
@@ -0,0 +1,3 @@
+return {
+  clojure = { 'clj-kondo' },
+}
---
+++ lua/custom/plugins/clojure-nvim.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    'Olical/conjure',
+    ft = { 'clojure', 'fennel' },
+    init = function()
+      vim.g['conjure#mapping#doc_word'] = false
+      vim.g['conjure#mapping#doc_word'] = 'gk'
+
+      vim.g['conjure#client#clojure#nrepl#mapping#session_clone'] = 'Sc'
+      vim.g['conjure#client#clojure#nrepl#mapping#session_fresh'] = 'Sf'
+      vim.g['conjure#client#clojure#nrepl#mapping#session_close'] = 'Sq'
+      vim.g['conjure#client#clojure#nrepl#mapping#session_close_all'] = 'SQ'
+      vim.g['conjure#client#clojure#nrepl#mapping#session_list'] = 'Sl'
+      vim.g['conjure#client#clojure#nrepl#mapping#session_next'] = 'Sn'
+      vim.g['conjure#client#clojure#nrepl#mapping#session_prev'] = 'Sp'
+      vim.g['conjure#client#clojure#nrepl#mapping#session_select'] = 'Ss'
+
+      vim.g['conjure#client#clojure#nrepl#connection#auto_repl#cmd'] = 'lein repl :headless'
+    end,
+  },
+  {
+    'clojure-vim/vim-jack-in',
+    dependencies = {
+      'tpope/vim-dispatch',
+      'radenling/vim-dispatch-neovim',
+    },
+    init = function()
+      vim.g['default_lein_task'] = 'repl :headless'
+    end,
+  },
+}
