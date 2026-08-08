08846687bf9067c2d1a1c22a9e05c15a5c08fcaf
Edgar Roman Cervantes <redvant@outlook.com>
2026-08-08 15:48:28 -0600

    Remove Comment.nvim

--- lua/custom/plugins/general-plugins.lua
+++ lua/custom/plugins/general-plugins.lua
@@ -2,10 +2,6 @@ return {
   'tpope/vim-dispatch',
   'HiPhish/rainbow-delimiters.nvim',
   {
-    'numToStr/Comment.nvim',
-    opts = {},
-  },
-  {
     'MeanderingProgrammer/render-markdown.nvim',
     ft = 'markdown',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
