f20bbc639b8bdd68904082f95b8ef86cf9df32c9
Edgar Roman Cervantes <redvant@outlook.com>
2026-08-09 20:15:34 -0600

    Change theme to Eldritch

--- init.lua
+++ init.lua
@@ -936,10 +936,22 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
-
+  {
+    'eldritch-theme/eldritch.nvim',
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('eldritch').setup {
+        transparent = true,
+      }
+      vim.cmd.colorscheme 'eldritch'
+    end,
+  },
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
